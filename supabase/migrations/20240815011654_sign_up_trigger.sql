477adf578e06596063b296cceb1761591e581ea1
maquico <1104666@est.intec.edu.do>
2024-08-14 21:17:52 -0400

    feat!(001): sign up trigger

---
+++ supabase/migrations/20240815011654_sign_up_trigger.sql
@@ -0,0 +1,24 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+ SET search_path TO ''
+AS $function$
+begin
+  insert into public.Usuarios (
+    "Usuario_ID",
+    nombre,
+    apellido,
+    correo)
+  values (new.id,
+   new.raw_user_meta_data ->> 'first_name',
+   new.raw_user_meta_data ->> 'last_name',
+   new.email);
+  return new;
+end;
+$function$
+;
+
+
