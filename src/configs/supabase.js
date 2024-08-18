00af38222550128dac4d5e296f0304e5025b94bc
maquico <1104666@est.intec.edu.do>
2024-08-17 22:26:07 -0400

    chore(db): local and online supabase config

--- src/configs/supabase.js
+++ src/configs/supabase.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.LOCAL_SUPABASE_URL || process.env.SUPABASE_URL;
+const supabaseKey = process.env.LOCAL_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY;
+
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
