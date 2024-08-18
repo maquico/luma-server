67f0b31b335910b5e0db135eedc916f555c0a21b
maquico <1104666@est.intec.edu.do>
2024-08-17 22:26:56 -0400

    fix(api): swagger config route

--- src/configs/swagger.js
+++ src/configs/swagger.js
@@ -8,7 +8,7 @@ const doc = {
 };
 
 const outputFile = './swagger-output.json';
-const routes = ['../routes/user.router.js'];
+const routes = ['../../server.js'];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
