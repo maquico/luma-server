7811fd18b8d4d93afa2b9759c4cdeb4565cde2cb
maquico <1104666@est.intec.edu.do>
2024-09-01 23:50:01 -0400

    refactor: session REST adaptation

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -1,10 +1,10 @@
 import session from '../services/session.service.js';
 
 // Controller using login service with try catch for error handling
-const login = async (req, res) => {
+const create = async (req, res) => {
     try {
         const { email, password } = req.body;
-        const { data, error } = await session.login(email, password);
+        const { data, error } = await session.create(email, password);
         if (error) {
             return res.status(400).send(error.message);
         }
@@ -15,5 +15,5 @@ const login = async (req, res) => {
 };
 
 export default {
-    login,
+    create,
 };
