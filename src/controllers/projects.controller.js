eb3650fc7471db069a01a2ceda4edbc35da44390
Wolbik <1105721@est.intec.edu.do>
2024-09-05 09:06:47 -0400

    feat(011): projects routes

---
+++ src/controllers/projects.controller.js
@@ -0,0 +1,36 @@
+import project from '../src/services/projects.service.js';
+
+// Controller using login service with try catch for error handling
+const create = async (req, res) => {
+    try {
+        const { nombre, descripcion } = req.body;
+        const { data, error } = await project.create(nombre, descripcion);
+        if (error) {
+            const errorStatusCode = parseInt(error.status, 10)
+            console.log(errorStatusCode);
+            return res.status(errorStatusCode).send(error.message);
+        }
+        return res.status(200).send(data);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+};
+
+const getProyectos = async (res) => {
+    try {
+        const { data, error } = await project.getProyectos();
+        if (error) {
+            const errorStatusCode = parseInt(error.status, 10)
+            console.log(errorStatusCode);
+            return res.status(errorStatusCode).send(error.message);
+        }
+        return res.status(200).send(data);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+export default {
+    create,
+    getProyectos,
+};  
---
+++ src/routes/projects.router.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import projectsController from '../controllers/projects.controller.js';
+
+const router = Router();
+router.post("/", projectsController.create);
+router.get("/", projectsController.getProyectos);
+
+export default router;
