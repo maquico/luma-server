46340dff87ea8cc068ed17d27fedd98a388371a1
maquico <1104666@est.intec.edu.do>
2024-09-14 17:44:36 -0400

    chore(db): not nullable user id on history tables

---
+++ supabase/migrations/20240914214225_historic_not_nullable_uid.sql
@@ -0,0 +1,10 @@
+alter table "public"."Historial_Fuentes" alter column "Usuario_ID" set not null;
+
+alter table "public"."Historial_Recompensas" alter column "Usuario_ID" set not null;
+
+alter table "public"."Historial_Temas" alter column "Usuario_ID" set not null;
+
+
+
+
+
