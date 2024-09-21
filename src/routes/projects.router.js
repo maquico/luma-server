263d94b8618f2ecf78305f39001cbe865109e3f2
maquico <1104666@est.intec.edu.do>
2024-09-21 18:01:15 -0400

    fix: routes fixing

--- src/routes/projects.router.js
+++ src/routes/projects.router.js
@@ -4,7 +4,7 @@ import projectsController from '../controllers/projects.controller.js';
 const router = Router();
 router.post("/", projectsController.create);
 router.get("/", projectsController.getProyectos);
-router.get("/:id", projectsController.getById);
-router.get("/:userId", projectsController.getByUser);
+router.get("/id/:id", projectsController.getById);
+router.get("/user/:userId", projectsController.getByUser);
 
 export default router;
